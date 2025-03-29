3db867604216430b811f7cf8c7679a9859781e24
zidhartha <datojincharadze26@gmail.com>
2025-03-30 02:30:08 +0400

    updated the home shit

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,11 +1,23 @@
-import React, { useState } from 'react'
-import './home.css'
+import React from "react";
+import "./Home.css";
 
 export default function Home() {
+  return (
+    <div className="home-container">
+      {/* Hero Image Section - Add this below your navbar */}
+      <div className="hero-image-container">
+        <img
+          src="/public/b.png"  // Your image path
+          alt="Algorithm Visualization"
+          className="hero-image"
+        />
+      </div>
 
-    return (
-        <div className='home'>
-            <h1>Home</h1>
-        </div>
-    )
+      {/* Keep your existing homepage content */}
+      <div className="content">
+        <h1>Welcome to Whitebridge</h1>
+        <p>Select an algorithm from the navigation bar above.</p>
+      </div>
+    </div>
+  );
 }
